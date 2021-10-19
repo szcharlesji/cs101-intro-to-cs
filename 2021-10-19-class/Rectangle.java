b5c892b06361b7004eef7cad5ebaf4d23c4cb825
Cheng Ji <szcharlesji@gmail.com>
2021-10-19 13:21:53 -0400

    update class

---
+++ 2021-10-19-class/Rectangle.java
@@ -0,0 +1,50 @@
+public class Rectangle
+{
+    private double length;
+    private double width;
+    private String color;
+
+    public Rectangle()
+    {
+        length = 1;
+        width = 2;
+        color = "Blue";
+    }
+
+    public Rectangle(int len, int wid, String col)
+    {
+        length = len;
+        width = wid;
+        color = col;
+    }
+
+    public void setLength(int len)
+    {
+        length = len;
+    }
+
+    public void setWidth(int wid)
+    {
+        width = wid;
+    }
+
+    public void setColor(String col)
+    {
+        color = col;
+    }
+
+    public double getLength()
+    {
+        return length;
+    }
+
+    public double getWidth()
+    {
+        return width;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+}
---
+++ 2021-10-19-class/RectangleDemo.java
@@ -0,0 +1,8 @@
+public class RectangleDemo {
+    public static void main(String[] args)
+    {
+        Rectangle r1 = new Rectangle();
+
+        r1.setColor("Brown");
+    }
+}
